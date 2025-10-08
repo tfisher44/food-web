2feb2af4901698e4bee2f7f7727af86389a7ba31
rmkelley-18 <rmkell8641@gmail.com>
2025-10-07 21:37:31 -0700

    added Calendar Components

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,9 +1,17 @@
+import HeroSection from "../components/HeroSection"
+import CalendarComponent from "../components/calendar_components/CalendarComponent";
 function CalendarPage(){
     return (
         <>
-            <p>Coming Soon!</p>
+            <HeroSection
+                title="Discover Events, Workshops, and Farmers Markets"
+                image="/assets/pictures/MapPageImage.webp"
+            />
+            <CalendarComponent/>
+            
         </>
     );
 }
+
 
 export default CalendarPage;
