e0a4ffc76da64cdb0e7611c2246a2c0301a30f9d
isajunio <Isabellejunio8@gmail.com>
2025-10-09 14:05:42 -0700

    New About Us Page
    Simply a new page with the HeroSection (gonna need to add a new picture)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar(){
             {/* <h3 className="food-web-logo">Put Logo Here</h3> */}
             <ul>
                 <li><Link to="/">Home</Link></li>
+                <li><Link to="/about-page">About</Link></li>
                 <li><Link to="/map-page">Map</Link></li>
                 <li><Link to="/calendar-page">Calendar</Link></li>
                 <li><Link to="/projects-page">Projects</Link></li>
