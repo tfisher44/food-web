a9c62a7b84a4cf891afd5952b8dbebd85a3a7a97
tfisher44 <taylorfisher9045@gmail.com>
2025-11-18 18:49:17 -0700

    Added content to the About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 // import components
 import HeroSection from "../components/HeroSection"
+import "./AboutPage.css"
 
 function AboutPage(){
     return (
@@ -8,8 +9,10 @@ function AboutPage(){
                title="About Us"
                image="/assets/pictures/HomePageImage.webp"
             />
-
-            {/* Main Content Area */}
+            <div id="our-story">
+                <h1>Our Story</h1>
+                <p>Food Web began with two ASU students and passionate gardeners who started volunteering at Encounter Farm and soon found a home in the community, land, and fresh produce that it offered. Curious about how many other community gardens were hiding in plain sight, they set out to discover as many as possible and quickly realized that there were dozens nearby, if only one knew where to look. Troubled by food deserts, food waste, and a food system that has largely overlooked local producers and the power of community, they set out to make a difference. With just a handful of ideas and a vision for a better future, they entered their project into the EPICS (Engineering Projects in Community Service) program at ASU. Two teams were formed: the Food Team, to build a water storage and drip irrigation system for Encounter Farm, and the Web Team, to develop this platform to help connect communities with their local food systems. Like a seed planted in soil, with time, care, and collaboration, the Food Web began to grow.</p>
+            </div>
         </>
     );
 }
