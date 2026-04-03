cc35656474147e91ab034b7b423418cea4ffe808
psakurai2 <147210537+psakurai2@users.noreply.github.com>
2026-04-03 16:45:25 -0700

    Changed home hero section video link to vimeo link. Also changed some of the code in HomeHeroSection.css to keep video centered

--- src/components/HomeHeroSection.jsx
+++ src/components/HomeHeroSection.jsx
@@ -3,15 +3,20 @@ import "./HomeHeroSection.css"
 function HomeHeroSection() {
   return (
     <section className="home-hero-section">
-      <video className="hero-video" autoPlay loop muted playsInline>
-        <source src="../../public/assets/videos/encounter-compressed.mp4" type="video/mp4" />
-      </video>
+      <iframe
+        className="hero-video"
+        src="https://player.vimeo.com/video/1180001288?badge=0&autopause=0&autoplay=1&muted=1&loop=1&background=1&player_id=0&app_id=58479"
+        allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+        allowFullScreen
+        referrerPolicy="strict-origin-when-cross-origin"
+        title="hero-video"
+      />
 
-      <div>
+      <div className="home-hero-content">
         <h1 className="home-hero-title">Food Web</h1>
       </div>
     </section>
   );
 }
 
-export default HomeHeroSection; 
+export default HomeHeroSection;
